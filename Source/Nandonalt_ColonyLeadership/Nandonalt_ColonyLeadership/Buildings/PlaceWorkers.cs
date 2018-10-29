48c20b935b38b54fdb907b9f771fe26b932e0f0e
Jdalt40 <Daltonjosh5@gmail.com>
2018-10-29 13:48:52 +1000

    First Commit

---
+++ Source/Nandonalt_ColonyLeadership/Nandonalt_ColonyLeadership/Buildings/PlaceWorkers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace Nandonalt_ColonyLeadership
+{
+    public class PlaceWorker_TeachingSpot : PlaceWorker
+    {
+
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null)
+        {
+            List<Thing> allBuildingsColonist = base.Map.listerThings.AllThings;
+            for (int i = 0; i < allBuildingsColonist.Count; i++)
+            {
+                Thing thing = allBuildingsColonist[i];
+                if (thing.def.defName == "TeachingSpot" || thing.def.defName == "TeachingSpot_Blueprint")
+                {
+                    return new AcceptanceReport(reasonText: "OnlyOnePerColony".Translate(new object[] { thing.def.LabelCap }));
+                }
+            }
+
+            return true;
+        }
+
+        public override void DrawGhost(ThingDef def, IntVec3 center, Rot4 rot, Color ghostCol)
+        {
+            GenDraw.DrawFieldEdges(WatchBuildingUtility.CalculateWatchCells(def, center, rot, map).ToList<IntVec3>());
+
+        }
+
+        public class PlaceWorker_BallotBox : PlaceWorker
+        {
+
+            public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null)
+            {
+
+                List<Thing> allBuildingsColonist = base.Map.listerThings.AllThings;
+                for (int i = 0; i < allBuildingsColonist.Count; i++)
+                {
+                    Thing thing = allBuildingsColonist[i];
+                    if (thing.def.defName == "BallotBox" || thing.def.defName == "BallotBox_Blueprint")
+                    {
+                        return new AcceptanceReport("OnlyOnePerColony".Translate(new object[] { thing.def.LabelCap }));
+                    }
+                }
+
+                return true;
+            }
+
+        }
+    }
+}
+
